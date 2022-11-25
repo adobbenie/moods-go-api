92fc40fe26701ae564f52a22139bf994868ded5d
Arthur Dobbenie <arthur.dobbenie@student.howest.be>
2022-11-25 20:04:04 +0100

    type structs in models file

---
+++ model/models.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primtive"
+)
+
+type song struct {
+	SpotifyID string `json:"spotify_id"`
+	TrackName string `json:"track_name"`
+	Artist    string `json:"artist"`
+	Duration  int    `json:"duration"`
+}
+
+// omitempty = empty values not allowed
+
+type user struct {
+	ID       primtive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	MarsID   int               `json:"mars_id"`
+	Username string            `json:"username"`
+	Moodlist []song            `json:"mood_list"`
+}
