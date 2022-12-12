833cf3d4afbae7ac112830b0604ac4c4720e0244
Arthur Dobbenie <arthur.dobbenie@student.howest.be>
2022-12-12 12:41:40 +0100

    changed database file to use .env files values

--- db/mongodb.connection.go
+++ db/mongodb.connection.go
@@ -4,20 +4,31 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const connectionString_ = "mongodb+srv://user:user@moodsmusicdb.teextgp.mongodb.net/?retryWrites=true&w=majority"
-const DbName = "moodsdb"
+// const connectionString_ = "mongodb+srv://user:user@moodsmusicdb.teextgp.mongodb.net/?retryWrites=true&w=majority"
+//const DbName = "moodsdb"
 
 var UserCollection *mongo.Collection
 var PubRoomCollection *mongo.Collection
 var PriRoomCollection *mongo.Collection
 
 func init() {
-	clientOption := options.Client().ApplyURI(connectionString_)
+	//connect to MongoDB Atlas
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("Your 'MONGODB_URI' environmental variable must be set by giving your connection string as it's value in the .env file.")
+	}
+
+	clientOption := options.Client().ApplyURI(uri)
 	client_, err := mongo.Connect(context.TODO(), clientOption)
 
 	if err != nil {
@@ -25,8 +36,14 @@ func init() {
 	}
 	fmt.Println("succesfully connected to mongodb")
 
-	UserCollection = client_.Database(DbName).Collection("users")
-	PubRoomCollection = client_.Database(DbName).Collection("public_rooms")
-	PriRoomCollection = client_.Database(DbName).Collection("private_rooms")
-	fmt.Println("All collection found")
+	//connect to database itself
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		log.Fatal("Your 'DB_NAME' environmental variable must be set by giving setting the name of your database as it's value in the .env file.")
+	}
+
+	UserCollection = client_.Database(dbName).Collection("users")
+	PubRoomCollection = client_.Database(dbName).Collection("public_rooms")
+	PriRoomCollection = client_.Database(dbName).Collection("private_rooms")
+	fmt.Println("All collections found")
 }
