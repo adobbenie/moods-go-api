3bb3619bae9c97a884a68a2b511ee5ae7c3835d6
Arthur Dobbenie <arthur.dobbenie@student.howest.be>
2022-12-12 14:09:16 +0100

    get all exisiting private rooms function

--- controller/priRoomController.go
+++ controller/priRoomController.go
@@ -1,9 +1,29 @@
 package controller
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"main.go/db"
+)
+
+var ctx = context.Background()
 
 func GetAllPrivateRooms(w http.ResponseWriter, r *http.Request) {
+	cursor, err := db.PriRoomCollection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var rooms []bson.M
+	if err = cursor.All(ctx, &rooms); err != nil {
+		log.Fatal(err)
+	}
 
+	w.Header().Set("Content-Type", "application/vnd.api+json")
+	json.NewEncoder(w).Encode(rooms)
 }
 
 func GetPrivateRoomById(w http.ResponseWriter, r *http.Request) {
--- router/router.go
+++ router/router.go
@@ -13,17 +13,17 @@ func Router() *mux.Router {
 	router.HandleFunc("/api/user", c.GetUserById).Methods("GET")     //marsId as query string parameters
 	router.HandleFunc("/api/user/data", c.UpdateUser).Methods("PUT") //complete user object in body (changeable field: username, spotify_user_id)
 
-	//LIST ROUTES
+	//MOODLIST ROUTES
 	router.HandleFunc("/api/user/list", c.UpdateMoodList).Methods("PUT") //only moodlist in body => array of songs
 
-	//PUBLIC ROOM ROUTES
+	//PRIVATE ROOM ROUTES
 	router.HandleFunc("/api/rooms/private", c.GetAllPrivateRooms).Methods("GET")
 	router.HandleFunc("/api/rooms/private/{id}", c.GetPrivateRoomById).Methods("GET")
 	router.HandleFunc("/api/rooms/private", c.CreateNewPrivateRoom).Methods("POST")
 	router.HandleFunc("/api/rooms/private", c.UpdatePrivateRoom).Methods("PUT")
 	router.HandleFunc("/api/rooms/private/{id}", c.DeletePrivateRoom).Methods("DELETE")
 
-	//PRIVATE ROOM ROUTES
+	//PUBLIC ROOM ROUTES
 	router.HandleFunc("/api/rooms/public", c.GetPublicRoomById).Methods("GET")    //id added as string query param.
 	router.HandleFunc("/api/rooms/public", c.CreateNewPublicRoom).Methods("POST") //room data as JSON in body (raspid, location, description)
 	router.HandleFunc("/api/rooms/public", c.DeletePublicRoom).Methods("DELETE")  //id added as string query param.
